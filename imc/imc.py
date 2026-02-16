5f17b072d5c670afbb27b6a7fa523407c4d94bc4
arielsilva12 <ariel.francisco12344@gmail.com>
2026-02-16 11:36:42 -0300

    Remove exercícios extras que não fazem parte da Atividade A

--- imc/imc.py
+++
@@ -1,7 +0,0 @@
-# imc é calculado dividindo o peso pela altura ao quadrado
-
-peso = float(input('Digite o seu peso em kg: '))
-altura = float(input('Digite a sua altura em metros: '))
-
-imc = peso / (altura **2)
-print(f'O seu IMC é: {imc}')
